04fb2ff2e0482491e6b475bfe5c0d341b48c5b49
arqady01 <36727217+arqady01@users.noreply.github.com>
2025-10-14 21:26:21 +0800

    Initial Commit

---
+++ lovpicTests/lovpicTests.swift
@@ -0,0 +1,17 @@
+//
+//  lovpicTests.swift
+//  lovpicTests
+//
+//  Created by mengfs on 10/14/25.
+//
+
+import Testing
+@testable import lovpic
+
+struct lovpicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
